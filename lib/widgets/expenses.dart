0cb0fc6ff66b07644516fbc6b181da1f2805cbb8
Harsh Anand <harsh250600@gmail.com>
2023-08-07 15:02:22 +0530

    date-picker module added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker_app/models/expense.dart';
 import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker_app/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -18,16 +19,32 @@ class _ExpenseState extends State<Expenses> {
       category: Category.work,
     ),
     Expense(
-      title: 'Flutter course',
+      title: 'Cinema',
       amount: 15.69,
       date: DateTime.now(),
       category: Category.leisure,
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The Chart'),
